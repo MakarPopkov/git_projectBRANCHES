8af087ebcafb454ba18556c6fcd02118481c78c9
Makar_Popkov <zadi.super.99@mail.ru>
2023-11-22 15:28:42 +0300

    third commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print('My first git program')
     print('And I change it everyday')
 
+def my_function():
+    print('Нужно решать задачи')
 
 if __name__ == '__main__':
     main()
