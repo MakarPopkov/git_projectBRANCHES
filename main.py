839b3c824a7552be9a1d05e67d96d3bddc887f9a
Makar_Popkov <zadi.super.99@mail.ru>
2023-11-22 15:42:25 +0300

    new branch

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     print('My first git program')
     print('And I change it everyday')
 
+
 def my_function():
-    print('Нужно решать задачи')
+    print('new branch')
+
 
 if __name__ == '__main__':
     main()
